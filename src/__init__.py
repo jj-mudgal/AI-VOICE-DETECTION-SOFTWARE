e4eb03532135943bc6f317cc5585e3a2eab82394
Janmejai Mudgal <janmejai345@gmail.com>
2026-02-17 16:56:06 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,5 @@
+"""
+AI Generated Voice Detector Package
+"""
+
 __version__ = "1.0.0"
-__author__ = "Janmejai Mudgal"
-__email__ = "janmejai345@gmail.com"
-
-from .aad.model import AudioDetector
-from .aad.infer import infer
-
-
